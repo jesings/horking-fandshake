515e251ec9fb8ebe84b7bb913a3da18891f41745
jesings <jsinger@stuy.edu>
2018-12-20 10:49:03 -0500

    Added error handling in handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -13,17 +13,35 @@
 int server_handshake(int *to_client) {
     puts("Server establishing connection to client");
     int wkp = open("Gandalf",O_RDONLY);
+    if(!wkp){
+        puts("Failure in opening well known pipe, make sure pipe exists");
+        exit(0);
+    }
     char pid[10];
-    read(wkp,pid,HANDSHAKE_BUFFER_SIZE);
+    if(!read(wkp,pid,HANDSHAKE_BUFFER_SIZE)){
+        puts("Failure in reading from pipe, make sure pipe exists");
+        exit(-1);
+    }
     puts("Server recieved private pipe name");
     *to_client = open(pid,O_WRONLY);
+    if(!*to_client){
+        puts("Failure in opening private pipe, make sure pipe exists");
+        exit(-1);
+    }
     close(wkp);
     sprintf(pid,"%d",getpid());
     mkfifo(pid,0644);
     puts("Connection established 1");
-    write(*to_client,pid,HANDSHAKE_BUFFER_SIZE);
+    if(!write(*to_client,pid,HANDSHAKE_BUFFER_SIZE)){
+        puts("Failure in writing to private pipe, make sure pipe exists");
+        exit(-1);
+    }
     puts("Opening server private pipe");
     int upstream = open(pid,O_RDONLY);
+    if(!upstream){
+        puts("Failure in opening server private pipe, make sure pipe exists");
+        exit(-1);
+    }
     puts("Acknowledge having recieved the connection");
     return upstream;
 }
@@ -43,14 +61,32 @@ int client_handshake(int *to_server) {
     sprintf(pid,"%d",getpid());
     mkfifo(pid,0644);
     int wrfd = open("Gandalf",O_WRONLY);
+    if(!wrfd){
+        puts("Failure in opening WKP client side");
+        exit(-1);
+    }
     puts("Client giving private pipe name to server");
-    write(wrfd,pid,HANDSHAKE_BUFFER_SIZE);
+    if(!write(wrfd,pid,HANDSHAKE_BUFFER_SIZE)){
+        puts("Failure in writing to WKP client side");
+        exit(-1);
+    }
     puts("Client establishing a connection with server");
     int downstream = open(pid,O_RDONLY);
+    if(!downstream){
+        puts("Failure in opening private pipe client side");
+        exit(-1);
+    }
     puts("Connection established");
     char ackwait[10];
-    read(downstream,ackwait,HANDSHAKE_BUFFER_SIZE);
+    if(!read(downstream,ackwait,HANDSHAKE_BUFFER_SIZE)){
+        puts("Failure in opening private pipe client side");
+        exit(-1);
+    }
     puts("Client recieved server private pipe");
     *to_server = open(ackwait,O_WRONLY);
+    if(!*to_server){
+        puts("Failure in opening private pipe client side");
+        exit(-1);
+    }
     return downstream;
 }
